b5ca49dee71f8ebb7e392029fe7f0d287a9a7473
zeero1q <shrikantvishwakrma74@gmail.com>
2020-10-26 05:25:38 +0530

    initial project files with configuration to the eureka server. closes #1

---
+++ src/main/java/com/zeeroiq/eureka/BreweryEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.zeeroiq.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class BreweryEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zeeroiq/eureka/BreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeeroiq.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
