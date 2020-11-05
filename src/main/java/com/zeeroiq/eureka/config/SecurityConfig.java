cee2c42c09b6dac29a55c57537e23c28bf6638eb
zeero1q <shrikantvishwakrma74@gmail.com>
2020-11-06 02:24:33 +0530

    spring security basic authentication configuration. closes #4

---
+++ src/main/java/com/zeeroiq/eureka/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+/*
+ * Created by zeeroiq on 11/6/20, 2:14 AM
+ */
+
+/*
+ * Created by zeeroiq on 11/6/20, 2:14 AM
+ */
+
+package com.zeeroiq.eureka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
