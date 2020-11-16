b6a70125a8288a799cbaeb47e371de734f680fcc
zeero1q <shrikantvishwakrma74@gmail.com>
2020-11-16 22:59:39 +0530

    added gradle configuration for building docker images and Dockerfile for the same is added. closes #5, closes #6.

--- src/main/java/com/zeeroiq/eureka/BreweryEurekaApplication.java
+++ src/main/java/com/zeeroiq/eureka/BreweryEurekaApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class BreweryEurekaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BreweryEurekaApplication.class, args);
     }
--- src/main/java/com/zeeroiq/eureka/config/SecurityConfig.java
+++ src/main/java/com/zeeroiq/eureka/config/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
